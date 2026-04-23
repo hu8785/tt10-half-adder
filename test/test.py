5c4de7a75742e42e2950959d9f535101aac509c3
hu8785 <patilaku96@gmail.com>
2026-04-23 17:36:49 -0400

    Update test.py
    done

--- test/test.py
+++ test/test.py
@@ -1,20 +1,14 @@
-# SPDX-FileCopyrightText: © 2024 Tiny Tapeout
-# SPDX-License-Identifier: Apache-2.0
-
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
-
 
 @cocotb.test()
 async def test_project(dut):
     dut._log.info("Start")
 
-    # Set the clock period to 10 us (100 KHz)
     clock = Clock(dut.clk, 10, units="us")
     cocotb.start_soon(clock.start())
 
-    # Reset
     dut._log.info("Reset")
     dut.ena.value = 1
     dut.ui_in.value = 0
@@ -23,18 +17,17 @@ async def test_project(dut):
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
 
-    dut._log.info("Test project behavior")
+    tests = [
+        ((0, 0), (0, 0)),
+        ((0, 1), (1, 0)),
+        ((1, 0), (1, 0)),
+        ((1, 1), (0, 1)),
+    ]
 
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
+    for (x, y), (S_exp, C_exp) in tests:
+        dut.ui_in[0].value = x
+        dut.ui_in[1].value = y
+        await ClockCycles(dut.clk, 2)
 
-    # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
-
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
-
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+        assert dut.uo_out[0].value == S_exp, f"Sum failed for x={x}, y={y}"
+        assert dut.uo_out[1].value == C_exp, f"Carry failed for x={x}, y={y}"
